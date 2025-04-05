3224e9e006a4a380b3a1bd20d543f7df2b100a2c
PiyushSahu <piyushsahuys@gmail.com>
2025-04-05 16:51:14 +0530

    structure for different landing page putting small components together

---
+++ Frontend/src/LandingPage/Pricing/Pricingpage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Hero from './Hero';
+import Brokerage from './Brokerage';
+import Footer from '../Footer';
+
+
+function Pricingpage() {
+    return ( 
+
+        <>
+        <Navbar/>
+        <Hero />
+        <Brokerage />
+        <Footer />
+        </>
+     );
+}
+
+export default Pricingpage;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import HomePage from './LandingPage/Home/Homepage'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>
+    
       <HomePage />
-    </div>
+    
   </StrictMode>,
 )
