368f4738b63e6827dde9202b046ae0f2d9beac01
PiyushSahu <piyushsahuys@gmail.com>
2025-04-05 16:22:32 +0530

    Renamed files to .jsx for Vite compatibility

---
+++ Frontend/src/LandingPage/OpenAcc.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function OpenAcc() {
+    return (  
+        <h1>Open Account</h1>
+    );
+
+<h1>Open Account</h1>}
+
+export default OpenAcc;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import HomePage from './LandingPage/Home/Homepage'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div>
-      <h1>hello</h1>
+      <HomePage />
     </div>
   </StrictMode>,
 )
